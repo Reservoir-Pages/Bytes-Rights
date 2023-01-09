ec4e4ca576fffe772c640b4a572149dd54ee877a
Nikitin Vladimir <vreservoirv@rambler.ru>
2023-01-09 21:55:20 +0300

    Production

--- src/scripts/components/index.js
+++ src/scripts/components/index.js
@@ -30,9 +30,7 @@ const feedbackBtns = document.querySelectorAll('.modal__btn');
 const closeBtns = document.querySelectorAll('.modal__close');
 modalBtns.forEach(btn => {
   btn.addEventListener('click', (e) => {
-
     let path = e.currentTarget.getAttribute('data-path');
-
 
     modalWindows.forEach(modal => {
       modal.classList.remove('modal__window--visible');
@@ -42,39 +40,38 @@ modalBtns.forEach(btn => {
     modalOverlay.classList.add('modal__overlay--visible');
     body.style.overflow = "hidden";
 
-
-    // document.querySelector(`[data-target="${path}"]`).querySelector('.modal__close');
-
-    // console.log(document.querySelector(`[data-target="${path}"]`).querySelector('.modal__close'));
+    setTimeout(() => {
+      document.querySelector(`[data-target="${path}"]`).querySelector('.modal__close').focus();
+    }, 100);
   });
 });
 modalOverlay.addEventListener('click', (e) => {
   if (e.target === modalOverlay) {
-    modalOverlay.classList.remove('modal__overlay--visible');
-    modalWindows.forEach(modal => {
-      modal.classList.remove('modal__window--visible');
-    });
-    body.style.overflow = "auto";
+    closeModal();
   };
 });
 feedbackBtns.forEach(btn => {
   btn.addEventListener('click', (e) => {
-    modalOverlay.classList.remove('modal__overlay--visible');
-    modalWindows.forEach(modal => {
-      modal.classList.remove('modal__window--visible');
-    });
-    body.style.overflow = "auto";
+    closeModal();
   });
 });
 closeBtns.forEach(btn => {
   btn.addEventListener('click', (e) => {
-    modalOverlay.classList.remove('modal__overlay--visible');
-    modalWindows.forEach(modal => {
-      modal.classList.remove('modal__window--visible');
-    });
-    body.style.overflow = "auto";
+    closeModal();
   });
 });
+document.addEventListener('keydown', (e) => {
+  if(e.code === 'Escape') {
+    closeModal();
+  }
+});
+function closeModal() {
+  modalOverlay.classList.remove('modal__overlay--visible');
+  modalWindows.forEach(modal => {
+    modal.classList.remove('modal__window--visible');
+  });
+  body.style.overflow = "auto";
+};
 
 // Send mail
 const form = document.querySelector('.form');
